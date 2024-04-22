fdae665d0283d98aa6f0dd2e56aab2b8f0e2822f
Mike Doan <mikedoan100@gmail.com>
2024-04-21 22:29:23 -0500

    update

--- js/gallery.js
+++ js/gallery.js
@@ -1,13 +1,9 @@
-function upDate(previewPic){
-
+function upDate(previewPic) {
     document.getElementById('image').innerHTML = previewPic.alt;
     document.getElementById('image').style.backgroundImage = "url(" + previewPic.src + ")";
-     
-       }
+}
    
-function unDo(){
-       
+function unDo() {   
     document.getElementById('image').style.backgroundImage = "url('')"
     document.getElementById('image').innerHTML = 'Hover over an image below to display here.';
-           
-       }
+}
